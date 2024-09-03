ce4771971b7a86de86d903b50446f1808b9259d9
bobser_mac <bobser223@users.noreply.github.com>
2024-09-03 16:41:58 +0300

    math task

---
+++ University/Labs/Discrete/Nums.cpp
@@ -0,0 +1,60 @@
+//
+// Created by Volodymyr Avvakumov on 03.09.2024.
+//
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+void find_max_num(vector<int> vec, string str, int &max_num, int &max_i){
+    int start;
+    // TODO: remake!!!!!!!!!
+    if (str[0] == 'r') {
+        max_num = vec[0];
+        max_i = 0;
+        start = 1;
+    } else {
+        max_num = vec[1];
+        max_i = 1;
+        start = 2:
+    }
+    for (int i = start; i < vec.size(); i++){
+        if (str[i] == 'r'){
+            if
+
+        } else if (str[i] == 'l') {
+
+
+        }
+
+    }
+
+
+}
+
+int foo(int num){
+    if (num == 1){
+        cout << 1 << endl;
+        return 0;
+    }
+    vector<int> vec;
+    string str;
+    for (int i = 1; i <= num; i++){
+        vec.push_back(i);
+        str += 'l';
+    }
+
+//    while (true){
+//
+//
+//
+//    }
+
+
+}
+
+
+int main(){
+
+    return 0;
+}
