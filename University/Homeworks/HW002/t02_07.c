2eb08ab435a71c5f712c2d4c1f4f810ce230d3ce
bobser_mac <bobser223@users.noreply.github.com>
2024-09-08 17:12:52 +0300

    Uni \ HW \ HW2 \t03_07.c + assertion

--- University/Homeworks/HW002/t02_07.c
+++ University/Homeworks/HW002/t02_07.c
@@ -3,9 +3,17 @@
 //
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
 double distance(double x1, double y1, double x2, double y2){
     return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+double el_area(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4){
+    assert((x1 == x3 && y1 == y3) && "Impossible input, monkey test failed");
+    double r = distance(x1, y1, x2, y2);
+    double R = distance(x3, y3, x4, y4);
+    return M_PI * r * R;
 }
 
 int main(){
@@ -14,8 +22,6 @@ int main(){
     printf("\n");
     scanf("%lf %lf %lf %lf", &x3, &y3, &x4, &y4);
     printf("\n");
-    double r = distance(x1, y1, x2, y2);
-    double R = distance(x3, y3, x4, y4);
-    printf("ellipse area = %lf", M_PI * r * R);
+    printf("ellipse area = %lf", el_area(x1, y1, x2, y2, x3, y3, x4, y4));
     return 0;
 }
